a6633c8a20a1789cf051d517f2c944968f32204f
NaviNaveenYadav <naveenvaddarapu4@gmail.com>
2023-11-20 13:32:39 +0000

    Initial Project

---
+++ src/test/java/testScripts/GooglePage.java
@@ -0,0 +1,23 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GooglePage {
+
+	public static void main(String[] args) {
+
+		// Token - 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
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+		WebElement srcBox = driver.findElement(By.id("APjFqb"));
+		srcBox.sendKeys("Java Tutorials");
+		srcBox.sendKeys(Keys.ENTER);
+	}
+
+}
